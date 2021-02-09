b8056a2fb1105bbff660f97f219a5d0212d5f288
Umar, Ismail Abiodun <ismail@ismailumar.com.ng>
2021-02-09 07:53:46 +0100

    Add project files.

---
+++ EvenOddSeparator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EvenOddSeparator
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               Console.WriteLine("Hello World!");
+               int[] numbers = new int[] {25, 47, 42, 56, 32};
+
+               var separator = new Separator();
+
+               Console.WriteLine(separator.Separate(numbers));
+
+               Console.ReadLine();
+          }
+     }
+
+     public class Separator
+     {
+          public string Separate(int[] numbers)
+          {
+               List<int> evenElements = new List<int>();
+               List<int> oddElements = new List<int>();
+
+               foreach (var t in numbers)
+               {
+                    if (t % 2 == 1)
+                    {
+                         oddElements.Add(t);
+                    }
+                    else
+                    {
+                         evenElements.Add(t);
+                    }
+               }
+               
+               return $"Even Elements: {string.Join(", ", evenElements)} | Odd Elements {string.Join(", ", oddElements)}";
+          }
+     }
+}
---
+++ TriangleArea/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TriangleArea
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               Console.WriteLine("Welcome to Math Algo");
+
+               Console.WriteLine("Please Enter the Value for A");
+               int a = Int32.Parse(Console.ReadLine());
+
+               Console.WriteLine("Please Enter the Value for B");
+               int b = Int32.Parse(Console.ReadLine());
+
+               Console.WriteLine("Please Enter the Value for C");
+               int c = Int32.Parse(Console.ReadLine());
+
+               var calculator = new Calculator();
+
+               Console.WriteLine($"Result => {calculator.CalculateTriangleArea(a, b, c)}");
+
+
+               Console.ReadLine();
+          }
+     }
+
+     class Calculator
+     {
+          public double CalculateTriangleArea(int a, int b, int c)
+          {
+               double result = 0;
+
+               double s = (a + b + c) / 2.0;
+
+               result = Math.Sqrt(s * ((s - a)  * (s - b) * (s - c)));
+
+               return result;
+          }
+     }
+}
